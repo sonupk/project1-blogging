5d0e16d3c25d9d4a25e06ba1c659fbd469242a27
KingK9t8 <ksagarm789@gmail.com>
2022-09-06 20:52:42 +0530

    DELETE-API

--- src/controllerK/controller.js
+++ src/controllerK/controller.js
@@ -17,10 +17,12 @@ const getBlogs = async function (req, res) {
 		const allBlogs = await BlogModel.find(obj);
 		console.log(allBlogs);
 		if (allBlogs.length === 0)
-			return res.status(404).send({ status: false, msg: "Resource Not Found" });
-		res.status(200).send({ status: true, data: allBlogs });
+			return res
+				.status(404)
+				.send({ status: false, msg: "return resource Not Found" });
+		return res.status(200).send({ status: true, data: allBlogs });
 	} catch (error) {
-		res.status(500).send({ status: false, msg: error.message });
+		return res.status(500).send({ status: false, msg: error.message });
 	}
 };
 
@@ -33,11 +35,14 @@ const deleteBlogById = async function (req, res) {
 				isDeleted: true,
 			}
 		);
+
 		return deletedBlog
 			? res.status(200).send()
-			: res.status(404).send({ status: false, msg: "Resource Not Found" });
+			: res
+					.status(404)
+					.send({ status: false, msg: "return resource Not Found" });
 	} catch (error) {
-		res.status(500).send({ status: false, msg: error });
+		return res.status(500).send({ status: false, msg: error });
 	}
 };
 
