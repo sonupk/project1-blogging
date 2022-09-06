dc7efef9be96b75153f0b1c7bd3252abffc2d72c
Parshantrao <parshantrao2017@gmail.com>
2022-09-06 11:24:54 +0530

    parshant

--- src/controller(P)/authorController.js
+++ src/controller(P)/authorController.js
@@ -1,23 +1,28 @@
 const authorModel = require("../models(P)/authorModel");
 
 const createAuthor = async function (req, res) {
-  const Data = req.body;
+  try{
+    const Data = req.body;
   // Destructuring
   const { fname, lname, title, email, password } = Data;
 
   //Validation starts
   //1.validation on fname
-  if (fname.trim().length == 0 || typeof fname != string) {
+  if (!fname||fname.trim().length == 0 || typeof fname != "string") {
     res.status(400).send({ status: false, msg: "fname is required" });
     return;
   }
+
+
   //2.validation on lname
-  if (lname.trim().length == 0 || typeof lname != string) {
+  if (!lname||lname.trim().length == 0 || typeof lname != "string") {
     res.status(400).send({ status: false, msg: "lname is required" });
     return;
   }
+
+
   //3.validation on title
-  if (title.trim().length == 0 || typeof title != string) {
+  if (!title||title.trim().length == 0 || typeof title != "string") {
     res.status(400).send({ status: false, msg: "title is required" });
     return;
   }
@@ -28,16 +33,22 @@ const createAuthor = async function (req, res) {
 
 
   //4.validation on email
+  if(!email){
+    res.status(400).send({ status: false, msg: "email must be present" });
+    return;
+  }
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!emailRegex.test(email) || !email) {
+  if (!emailRegex.test(email) ) {
     res.status(400).send({ status: false, msg: "email must be valid" });
     return;
   }
   const repeatEmail = await authorModel.find({email:email})
-  if(!repeatEmail){
+  if(!repeatEmail.length==0){
     res.status(400).send({status:false, msg:"email ID already exist"})
     return
   }
+
+
   //5.validation on password
   const passwordRegex =
     /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
@@ -48,14 +59,15 @@ const createAuthor = async function (req, res) {
     });
     return;
   }
-//   const repeatPass = await authorModel.find({password:password})
-//   if(!repeatPass){
-//     res.status(400).send({status:false, msg:"Invalid password"})
-//   }
   // Validation Ends
 
   const newAuthor = await authorModel.create(Data);
   res.status(201).send({ status: true, data: newAuthor });
+  }
+  catch(err){
+    res.status(500).send({status:false, msg:err.message})
+  }
+  
 };
 
 module.exports={createAuthor}
