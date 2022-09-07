db2cabd1ed3c77f9d510a4d3350e190076e4230a
KingK9t8 <ksagarm789@gmail.com>
2022-09-07 16:57:56 +0530

    Authentication

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+
 const authorController = require("../controller(P)/authorController");
 const blogController = require("../controller(P)/blogController");
 const getBlog = require("../controllerK/controller");
 const BlogValidation = require("../middlewareK/middleware");
+const authUser=require("../authK/auth")
+
+router.use("/blogs",authUser.userAuthentication)
 
 router.post("/authors", authorController.createAuthor);
 router.post("/blogs", blogController.createBlog);
 router.get("/blogs", BlogValidation.BlogValidationFromQuery, getBlog.getBlogs);
-router.delete("/blogs/:blogId", getBlog.deleteBlogById);
+router.delete("/blogs/:blogId",authUser.userAuthorisation, getBlog.deleteBlogById);
 
 module.exports = router;
