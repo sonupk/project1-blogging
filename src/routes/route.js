abfdcf3061a7f87d1caf4d6303613b6606897eb7
KingK9t8 <ksagarm789@gmail.com>
2022-09-08 09:53:08 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const authorController = require("../controller/authorController");
-const blogController = require("../controller/blogController");
-const getBlog = require("../controller/blogController(new)");
+const blogController = require("../controller/blogController(new)");
+const authorController = require("../controller/authorController(new)");
 const BlogValidation = require("../middleware/middleware");
 const authUser = require("../middleware/auth");
 
 router.use("/blogs", authUser.userAuthentication);
 
 router.post("/authors", authorController.createAuthor);
+router.post("/login", authorController.authorLogin);
 router.post("/blogs", authUser.userAuthorisation, blogController.createBlog);
 router.get("/blogs", BlogValidation.BlogValidationFromQuery, getBlog.getBlogs);
+router.put(
+	"/blogs/:blogId",
+	authUser.userAuthorisation,
+	blogController.updateBlog
+);
 router.delete(
 	"/blogs/:blogId",
 	authUser.userAuthorisation,
